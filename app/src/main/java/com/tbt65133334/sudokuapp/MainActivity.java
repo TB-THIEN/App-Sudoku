5484a75014eb1b8a8046e6b367c1787724f3bcc4
Trần Bảo Thiên <thanhthientran969@gmail.com>
2026-05-27 19:05:24 +0700

    up

--- app/src/main/java/com/tbt65133334/sudokuapp/MainActivity.java
+++ app/src/main/java/com/tbt65133334/sudokuapp/MainActivity.java
@@ -2,22 +2,57 @@ package com.tbt65133334.sudokuapp;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.tbt65133334.sudokuapp.ui.HomeFragment;
 import com.tbt65133334.sudokuapp.database.SudokuDatabase;
-import com.tbt65133334.sudokuapp.until.SudokuGenerator;
+import com.tbt65133334.sudokuapp.until.SudokuGenerator; // Lưu ý: nếu tên thư mục là 'util' thì bạn sửa lại chữ 'until' này nhé
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         initDatabase();
+
+        testFirebase();
+
         if (savedInstanceState == null) {
             navigateTo(new HomeFragment(), false);
         }
+    }
+
+    private void testFirebase() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 
     private void initDatabase() {
@@ -38,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                     writeableDb.setTransactionSuccessful();
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    Log.e(TAG, "Error initializing database", e);
                 } finally {
                     writeableDb.endTransaction();
                 }
@@ -54,7 +89,6 @@ public class MainActivity extends AppCompatActivity {
         return sb.toString();
     }
 
-    // ← Giữ nguyên 2 hàm cũ, không đổi gì
     public void navigateTo(Fragment fragment, boolean addToBackStack) {
         var tx = getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, fragment);
@@ -67,5 +101,4 @@ public class MainActivity extends AppCompatActivity {
                 androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE);
         navigateTo(new HomeFragment(), false);
     }
-
 }
